2c2562f0f8df2c2b0fbbaa4f4d58868a74a01d84
Moormaninmotion-byte <Moormaninmotion@gmail.com>
2025-10-04 02:01:32 -0400

    src to docs

--- vite.config.ts
+++ vite.config.ts
@@ -18,8 +18,8 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
+        '@': path.resolve(__dirname, 'docs/'),
+      }
     }
   };
 });
