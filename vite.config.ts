b3b3d4fffc0721b1c329ecf1e058417c0f89bfbf
moormaninmotion-byte <moormaninmotion@gmail.com>
2025-10-03 21:47:23 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const repoName = '/portfolio/';
+const repoName = '/Portfolio/';
 
 
 export default defineConfig(({ mode }) => {
