1f4c057d6bf72fdcf95e2addacdc46bd63e9bc9e
Moormaninmotion-byte <Moormaninmotion@gmail.com>
2025-10-03 22:00:33 -0400

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,24 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
-const repoName = '/Portfolio/';
-
+import { homepage } from './package.json';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
+  const base = process.env.NODE_ENV === 'production' ? new URL(homepage).pathname : '/';
   return {
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
     plugins: [react()],
-    base: process.env.NODE_ENV === 'production' ? repoName : '/',
+    base: base,
     build: {
       outDir: 'docs/',
     },
     resolve: {
       alias: {
-        '@': path.resolve(repoName, '.'),
+        '@': path.resolve(__dirname, './src'),
       },
     }
   };
