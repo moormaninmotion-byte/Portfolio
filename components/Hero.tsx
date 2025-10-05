44a3a10b59d500c1540a5eca8cf1963fbdcd6628
moormaninmotion-byte <moormaninmotion@gmail.com>
2025-10-04 23:43:14 -0400

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,17 +5,17 @@ export const Hero: React.FC = () => {
   return (
     <Section id="about" className="flex items-center min-h-[60vh]">
       <div className="grid grid-cols-1 md:grid-cols-5 gap-12 items-center">
-        <div className="md:col-span-2 flex justify-center order-1 md:order-1">
+        <div className="md:col-span-2 flex justify-center order-1 md:order-1 hover:border-4">
           <img
             src="https://i.ibb.co/YTpMXx41/RUID55dc386b036248bd9a970957aa215225.jpg"
             alt="Headshot"
-            className="object-cover border-44 border-neutral-800 transition-colors duration-300 hover:border-brand-accent shadow-lg"
+            className="object-cover border-4 border-neutral-800 transition-colors duration-300 hover:border-brand-accent shadow-lg"
             loading="lazy"
             decoding="async"
           />
         </div>
         <div className="md:col-span-3 text-center md:text-left order-2 md:order-2">
-          <h1 className="font-display text-5xl md:text-xl font-extrabold tracking-tighter leading-tight mb-6">
+          <h1 className="font-display text-5xl md:text-xl font-extrabold tracking-tighter leading mb-6">
             <span class Name = "hover:text-brand-accent">David</span>
             <br />
             <span className="text-brand-accent"> Moorman.</span>
